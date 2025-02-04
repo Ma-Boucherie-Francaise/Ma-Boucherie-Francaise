4341e3355953d5511d5a4feaeefcd710f16f38c7
Scharly <scharly.opitz@gmail.com>
2025-02-04 12:49:53 +0100

    Ajout de la page d'accueil et de sa feuille de stylz

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import Header from "./assets/components/Header";
+import Accueil from "./assets/pages/Accueil";
 
 export default function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route path="/" element={<Accueil />} />
+      </Routes>
     </>
   );
 }
