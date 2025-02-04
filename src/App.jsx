ac2baa3a18352b44108cb7ec2a6958c24c030608
Scharly <scharly.opitz@gmail.com>
2025-02-04 15:31:14 +0100

    Ajout de la page histoire aux routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./assets/components/Header";
 import Accueil from "./assets/pages/Accueil";
+import Histoire from "./assets/pages/Histoire";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Accueil />} />
+        <Route path="/histoire" element={<Histoire />} />
       </Routes>
     </>
   );
