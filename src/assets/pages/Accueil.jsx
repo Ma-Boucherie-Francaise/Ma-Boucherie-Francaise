aba215b47ccfe0318190b653703fc84ef57bb8b2
Scharly <scharly.opitz@gmail.com>
2025-02-04 14:33:19 +0100

    Ajout des pattern rouge pour la première section

--- src/assets/pages/Accueil.jsx
+++ src/assets/pages/Accueil.jsx
@@ -1,5 +1,6 @@
 import TitlePage from "../components/TitlePage";
 import ImageAccueil from "/ImageAccueil.jpg";
+import Pattern4Rouge from "/PatternRouge4.png";
 
 export default function Accueil() {
   return (
@@ -17,6 +18,15 @@ function FirstSection() {
     <section className="FirstSection">
       <div className="image">
         <img src={ImageAccueil} alt="image accueil" />
+      </div>
+      <div className="title">
+        <h1>Le goût de l'excellence,</h1>
+        <p>directement chez vous.</p>
+      </div>
+      <button>Découvrez nos viandes</button>
+      <div className="patternContainer">
+        <img src={Pattern4Rouge} alt="patternRouge" />
+        <img src={Pattern4Rouge} alt="patternRouge" />
       </div>
     </section>
   );
