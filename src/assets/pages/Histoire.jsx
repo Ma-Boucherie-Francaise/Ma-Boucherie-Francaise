0bc285e6b460fc09a7ff3412087ab07ebde8bc7a
Scharly <scharly.opitz@gmail.com>
2025-02-04 15:48:23 +0100

    Ajout du composent FirstSection

--- src/assets/pages/Histoire.jsx
+++ src/assets/pages/Histoire.jsx
@@ -1,10 +1,25 @@
 import TitlePage from "../components/TitlePage";
+import Image1 from "/ImageHistoire1.jpg";
 
 export default function Histoire() {
   return (
     <>
       <TitlePage title="Histoire —" />
-      <main>Histoire</main>
+      <main id="Histoire">
+        <FirstSection />
+      </main>
     </>
+  );
+}
+
+function FirstSection() {
+  return (
+    <section className="FirstSection">
+      <h1>Un métier passion</h1>
+      <div className="contentContainer">
+        <img src={Image1} alt="image histoire1" />
+        <div className="text"></div>
+      </div>
+    </section>
   );
 }
