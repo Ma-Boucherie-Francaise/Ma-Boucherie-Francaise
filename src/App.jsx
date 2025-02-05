ef4dbf5928f10319687559207c5117dfc7657103
Scharly <scharly.opitz@gmail.com>
2025-02-05 13:38:26 +0100

    Changement emplacement des fichiers

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import Header from "./assets/components/Header";
-import Accueil from "./assets/pages/Accueil";
-import Histoire from "./assets/pages/Histoire";
-import Produits from "./assets/pages/Produits";
+import Header from "./components/Header";
+import Accueil from "./pages/Accueil";
+import Histoire from "./pages/Histoire";
+import Produits from "./pages/Produits";
 
 export default function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./assets/style/style.css";
+import "./style/style.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
