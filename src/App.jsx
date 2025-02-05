2a271012ddd661ecec9010f9e88f14779546509a
Scharly <scharly.opitz@gmail.com>
2025-02-05 18:29:25 +0100

    Ajout de la restoration du scroll au changement de page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import Accueil from "./pages/Accueil";
 import Histoire from "./pages/Histoire";
 import Produits from "./pages/Produits";
+import { useEffect } from "react";
 
 export default function App() {
+  const { pathname } = useLocation();
+
+  const resetScroll = () => {
+    window.scrollTo(0, 0);
+  };
+
+  useEffect(() => {
+    resetScroll();
+  }, [pathname]);
+
   return (
     <>
       <Header />
