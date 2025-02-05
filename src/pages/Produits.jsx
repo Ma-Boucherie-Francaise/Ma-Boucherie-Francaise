a79b563e8215de1253c3523431d33feb3b3a6397
Scharly <scharly.opitz@gmail.com>
2025-02-05 17:28:30 +0100

    Ajout des animations au survols de chaque Catégories

--- src/pages/Produits.jsx
+++ src/pages/Produits.jsx
@@ -1,24 +1,42 @@
+import { useState } from "react";
 import ProduitsData from "../assets/ProduitsData.json";
 import TitlePage from "../components/TitlePage";
+import { motion as m } from "motion/react";
 
 export default function Produits() {
+  const [projectHovered, setProjectHovered] = useState();
+
   return (
     <>
       <TitlePage title="Produits —" />
       <main id="Produits">
-        <Pictograms />
-        <Texts />
+        <Pictograms setProjectHovered={setProjectHovered} />
+        <Texts projectHovered={parseInt(projectHovered)} />
       </main>
     </>
   );
 }
 
-function Pictograms() {
+function Pictograms({ setProjectHovered }) {
+  const MouseEnter = (e) => {
+    setProjectHovered(e.target.dataset.id);
+  };
+
+  const MouseLeave = () => {
+    setProjectHovered();
+  };
+
   return (
     <div className="pictoContainer">
       {ProduitsData.map(({ picto, name }, i) => {
         return (
-          <div key={i} className="picto">
+          <div
+            key={i}
+            data-id={i}
+            onMouseEnter={(e) => MouseEnter(e)}
+            onMouseLeave={() => MouseLeave()}
+            className="picto"
+          >
             <div className="image">
               <img src={picto} alt={`image ${name}`} />
             </div>
@@ -29,22 +47,38 @@ function Pictograms() {
   );
 }
 
-function Texts() {
+function Texts({ projectHovered }) {
+  const animeText = {
+    hide: {
+      y: "102%",
+      transition: { duration: 0.5, ease: [0.65, 0, 0.35, 1] },
+    },
+    show: {
+      y: 0,
+      transition: { duration: 0.5, ease: [0.65, 0, 0.35, 1] },
+    },
+  };
+
   return (
     <div className="textsContainer">
       {ProduitsData.map(({ name, underName, sentence }, i) => {
         return (
-          <div key={i} className="text">
+          <m.div
+            initial="initial"
+            animate={projectHovered === i ? "show" : "hide"}
+            key={i}
+            className="text"
+          >
             <div className="hidden">
-              <h1>{name}</h1>
+              <m.h1 variants={animeText}>{name}</m.h1>
             </div>
             <div className="hidden">
-              <p>{underName}</p>
+              <m.p variants={animeText}>{underName}</m.p>
             </div>
             <div className="hidden">
-              <span>{sentence}</span>
+              <m.span variants={animeText}>{sentence}</m.span>
             </div>
-          </div>
+          </m.div>
         );
       })}
     </div>
