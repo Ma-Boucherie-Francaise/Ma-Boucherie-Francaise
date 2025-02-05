0276b91077b104369ab472a9228af5291b534a94
Scharly <scharly.opitz@gmail.com>
2025-02-05 15:54:16 +0100

    Ajout de la page produits et sa Data

--- src/pages/Produits.jsx
+++ src/pages/Produits.jsx
@@ -1,3 +1,15 @@
+import ProduitsData from "../assets/ProduitsData.json";
+
 export default function Produits() {
-  return <main id="Produits">Produits</main>;
+  return (
+    <main id="Produits">
+      {ProduitsData.map(({ picto, name }, i) => {
+        return (
+          <div key={i} className="picto">
+            <img src={picto} alt={`image ${name}`} />
+          </div>
+        );
+      })}
+    </main>
+  );
 }
