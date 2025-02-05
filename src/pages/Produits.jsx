08934b3bee69e8d748ed5f246ea7f011ca9e0767
Scharly <scharly.opitz@gmail.com>
2025-02-05 16:33:12 +0100

    Ajout des titres de chaque catégories

--- src/pages/Produits.jsx
+++ src/pages/Produits.jsx
@@ -6,18 +6,47 @@ export default function Produits() {
     <>
       <TitlePage title="Produits —" />
       <main id="Produits">
-        <div className="pictoContainer">
-          {ProduitsData.map(({ picto, name }, i) => {
-            return (
-              <div key={i} className="picto">
-                <div className="image">
-                  <img src={picto} alt={`image ${name}`} />
-                </div>
-              </div>
-            );
-          })}
-        </div>
+        <Pictograms />
+        <Texts />
       </main>
     </>
+  );
+}
+
+function Pictograms() {
+  return (
+    <div className="pictoContainer">
+      {ProduitsData.map(({ picto, name }, i) => {
+        return (
+          <div key={i} className="picto">
+            <div className="image">
+              <img src={picto} alt={`image ${name}`} />
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function Texts() {
+  return (
+    <div className="textsContainer">
+      {ProduitsData.map(({ name, underName, sentence }, i) => {
+        return (
+          <div key={i} className="text">
+            <div className="hidden">
+              <h1>{name}</h1>
+            </div>
+            <div className="hidden">
+              <p>{underName}</p>
+            </div>
+            <div className="hidden">
+              <span>{sentence}</span>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
