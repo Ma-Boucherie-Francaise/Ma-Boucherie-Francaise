4bb7265dc249ab7f7511b518b0462181a0050ff6
Scharly <scharly.opitz@gmail.com>
2025-02-05 15:59:08 +0100

    Ajout du titre pour la page Produits

--- src/pages/Produits.jsx
+++ src/pages/Produits.jsx
@@ -1,15 +1,19 @@
 import ProduitsData from "../assets/ProduitsData.json";
+import TitlePage from "../components/TitlePage";
 
 export default function Produits() {
   return (
-    <main id="Produits">
-      {ProduitsData.map(({ picto, name }, i) => {
-        return (
-          <div key={i} className="picto">
-            <img src={picto} alt={`image ${name}`} />
-          </div>
-        );
-      })}
-    </main>
+    <>
+      <TitlePage title="Produits —" />
+      <main id="Produits">
+        {ProduitsData.map(({ picto, name }, i) => {
+          return (
+            <div key={i} className="picto">
+              <img src={picto} alt={`image ${name}`} />
+            </div>
+          );
+        })}
+      </main>
+    </>
   );
 }
