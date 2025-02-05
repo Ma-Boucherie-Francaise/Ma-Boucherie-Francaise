b7d609f2e5804e4aa87ec454b1dd5f0c5064cfff
Scharly <scharly.opitz@gmail.com>
2025-02-05 12:21:50 +0100

    Ajout des images pour la charte qualité pour la section 2

--- src/assets/pages/Histoire.jsx
+++ src/assets/pages/Histoire.jsx
@@ -1,5 +1,9 @@
 import TitlePage from "../components/TitlePage";
 import Image1 from "/ImageHistoire1.jpg";
+import PictoCircuit from "/PictoCircuitCourt.svg";
+import PictoFraicheur from "/PictoFraicheur.svg";
+import PictoSavoirFaire from "/PictoSavoirFaire.svg";
+import PictoEleveurs from "/PictoEleveurs.svg";
 
 export default function Histoire() {
   return (
@@ -7,6 +11,7 @@ export default function Histoire() {
       <TitlePage title="Histoire —" />
       <main id="Histoire" className="redColor">
         <FirstSection />
+        <SecondSection />
       </main>
     </>
   );
@@ -49,5 +54,42 @@ function FirstSection() {
 }
 
 function SecondSection() {
-  return <section className="SecondSection">Section 2</section>;
+  const cards = [
+    {
+      image: PictoEleveurs,
+      text: "Nous garantissons la provenance de nos viandes uniquement française et nous fournissons la traçabilité de tous nos produits. (100% de nos produits sont porteurs des logos viande de France*)",
+    },
+    {
+      image: PictoCircuit,
+      text: "LE RESPECT DU BIEN ÊTRE ANIMAL : l’élevage à taille humaine alimentation saine : Pour les bovins constituée d'herbe, pâturée ou en fourrage complétée avec des céréales. Pour les porcs, l'alimentation EST composée de céréales, oléoprotéagineux, vitamines et minéraux.des logements adapté ",
+    },
+    {
+      image: PictoFraicheur,
+      text: "la mise en place d’information de traçabilité et information nutritionnelle claire, lisible et facile à comprendre pour tous.",
+    },
+    {
+      image: PictoSavoirFaire,
+      text: "Fort d’un apprentissage rigoureux et de plusieurs années d’expérience, notre équipe est composée de passionnés maîtrisant le savoir-faire artisanal qui allie précision, respect du produit et amour du métier. Chaque pièce est travaillée avec soin, dans la pure tradition bouchère, pour garantir à nos clients une qualité irréprochable et un goût authentique.",
+    },
+  ];
+
+  return (
+    <section className="SecondSection">
+      <div className="title">
+        <h1>Charte Qualité</h1>
+        <p>nos engagements :</p>
+      </div>
+
+      <div className="cardsContainer">
+        {cards.map(({ image, text }, i) => {
+          return (
+            <div key={i} className="card">
+              <img src={image} alt={`Picto ${i + 1}`} />
+              <p>{text}</p>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 }
