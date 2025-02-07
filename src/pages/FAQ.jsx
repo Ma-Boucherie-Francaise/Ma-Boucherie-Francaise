57b1a945b91df95eecfb65deb8bf1762614ac121
Scharly <scharly.opitz@gmail.com>
2025-02-07 16:28:44 +0100

    Mise en page de la Faq

--- src/pages/FAQ.jsx
+++ src/pages/FAQ.jsx
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import TitlePage from "../components/TitlePage";
+import FaqData from "../assets/Faqata.json";
 
 export default function FAQ() {
+  const [clicked, setClicked] = useState();
+
   return (
     <>
       <TitlePage title="FAQ —" />
-      <main id="FAQ">FAQ</main>
+      <main id="FAQ">
+        <h1>Faq</h1>
+        {FaqData.map(({ title, questions }, i) => {
+          return (
+            <div key={i} className="faqBlockContainer">
+              <h2>{title}</h2>
+              {questions.map(({ question, answer }, i) => {
+                return (
+                  <div key={i} className="questionContainer">
+                    <div className="question">
+                      <p>{question}</p> <button />
+                    </div>
+                    {clicked === i && (
+                      <div className="answer">
+                        <p>{answer}</p>
+                      </div>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </main>
     </>
   );
 }
